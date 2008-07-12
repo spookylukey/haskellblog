4a2324da2e320ea778f4968e14e4bb8578fd4459
Luke Plant <L.Plant.98@cantab.net>
2008-07-12 20:40:42 +0100

    Removed unused settings

--- src/settingslocal.hs
+++ src/settingslocal.hs
@@ -1,13 +1,3 @@
 module Settings where
 
-dbhost = "localhost"
-dbname = "hdb_test_db"
-dbusername = "tester"
-dbpassword = "test"
-odbc_dsn="Driver={MySQL ODBC 3.51 Driver};Server=" ++ dbhost ++ 
-	";Database=" ++ dbname ++ 
-	"; User=" ++ dbusername ++ 
-	";Password=" ++ dbpassword ++ 
-	";Option=3;"
-
 sqlite_path="/home/luke/httpd/lukeplant.me.uk/web/cgi-bin/data/test1.db"

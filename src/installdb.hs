fb8e2d13f80c7e9871e0baeddc937d4a2394ea43
Luke Plant <L.Plant.98@cantab.net>
2008-07-28 19:14:18 +0100

    First stab at schema creation

--- src/installdb.hs
+++ src/installdb.hs
@@ -1,5 +1,3 @@
-import Network.CGI
-import Text.XHtml
 
 import Control.Exception
 import Control.Monad
@@ -7,83 +5,65 @@ import Database.HDBC
 
 import qualified DB
 
-msgpage :: String -> Html
-msgpage msg = body << paragraph << msg
-
-cgiMain :: CGI CGIResult
-cgiMain = 
-    do
-      sqlmsg <- liftIO runSql
-      output $ renderHtml $ msgpage sqlmsg
-
-
 main :: IO ()
-main = runCGI $ handleErrors cgiMain
---main = runSql >>= putStr
-  
-{-runSql' = do
-  c <- DB.connect
-  res <- quickQuery c "Select 1 + 1;" []
-  disconnect c
-  return $ show res
--}
+main = runSql >>= putStr
 
 runSql :: IO String
 runSql = catchDyn (do
 	             c <- DB.connect
-                     --dropTables c
-                     --commit c
 		     createTables c
                      commit c
-		     insertRecords c
-                     commit c
-		     rs <- retrieveRecords c
-                     return (
-			 " Records inserted in table Test are: \n" ++
-                         "*************************************\n" ++
-			 (show rs) ++ "\n" ++
-			 "*************************************\n\n" {- ++		
-			 " The tables in your database are:    "     ++ 
-			 "*************************************\n" ++
-			 (concatMap show mi) ++ "\n" ++
-			 "*************************************\n\n" -}
-                            )
+                     return "OK\n"
                   )
                    (\e -> return $ show (e :: SqlError))
 
-
 createTables :: IConnection conn => conn -> IO ()
 createTables c = do
-	run c "create table Test(id integer not null, name varchar(255) not null);" [] 
-        return ()
-
-dropTables :: IConnection conn => conn -> IO ()
-dropTables c = do
-	run c "drop table Test;" []
-        return ()
-	
-insertRecords :: IConnection conn => conn -> IO ()
-insertRecords c = do
-	run c "insert into Test(id,name) values (1,'Test1');" []
-	run c "insert into Test(id,name) values (2,'Test2');" []
-	run c "insert into Test(id,name) values (3,'Test3');" []
-	run c "insert into Test(id,name) values (4,'Test4');" []
-        return ()
-
-retrieveRecords :: IConnection conn => conn -> IO [(Int,String)]
-retrieveRecords c = do
-	liftM (map getRow) (quickQuery c "select id, name from Test;" [])
-	where
-		getRow row = (fromSql (row!!0), fromSql (row!!1))
-
-retrieveRecords' :: IConnection conn => conn -> IO [[SqlValue]] 
-retrieveRecords' c = quickQuery c "select 1 + 1;" [] 
-
-
-
-{-
-getMetaInfo :: IConnection conn => conn -> IO [(String,[FieldDef])]
-getMetaInfo c = do
-	ts <- tables c
-	mapM (\t -> describe c t >>= \cs -> return (t,cs)) ts
--}
+  let commands = 
+          ["\n\
+           \  CREATE TABLE metainfo (\n\
+           \    key TEXT,\n\
+           \    value TEXT\n\
+           \  );",
+           "\n\
+           \  CREATE TABLE formats (\n\
+           \    id INTEGER PRIMARY KEY,\n\
+           \    name TEXT,\n\
+           \    posts_enabled INTEGER,\n\
+           \    comments_enabled INTEGER\n\
+           \  );",
+           "\n\
+           \  CREATE TABLE posts (\n\
+           \    id INTEGER PRIMARY KEY AUTOINCREMENT,\n\
+           \    subject TEXT,\n\
+           \    post_raw TEXT,\n\
+           \    post_formatted TEXT,\n\
+           \    summary_raw TEXT,\n\
+           \    summary_formatted TEXT,\n\
+           \    format_id INTEGER REFERENCES formats(id),\n\
+           \    timestamp INTEGER,\n\
+           \    comments_open INTEGER\n\
+           \  );",
+           "\n\
+           \  CREATE TABLE categories (\n\
+           \    id INTEGER PRIMARY KEY AUTOINCREMENT,\n\
+           \    name TEXT\n\
+           \  );",
+           "\n\
+           \  CREATE TABLE post_categories (\n\
+           \    post_id INTEGER REFERENCES posts(id),\n\
+           \    category_id INTEGER REFERENCES categories(id)\n\
+           \  );",
+           "\n\
+           \  CREATE TABLE comments (\n\
+           \    id INTEGER PRIMARY KEY AUTOINCREMENT,\n\
+           \    post_id INTEGER REFERENCES posts(id),\n\
+           \    timestamp INTEGER,\n\
+           \    name TEXT,\n\
+           \    email TEXT,\n\
+           \    text_raw TEXT,\n\
+           \    text_formatted TEXT,\n\
+           \    format_id INTEGER REFERENCES format(id)\n\
+           \  ); \n\
+           \ "]
+  sequence_ $ map (\cmd -> run c cmd []) commands

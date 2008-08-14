b852d27a78123b88a02ffcb0485f07c389fa62bc
Luke Plant <L.Plant.98@cantab.net>
2008-08-14 22:22:39 +0100

    Convert function to layout instead of braces/semicolons

--- src/Blog/Post.hs
+++ src/Blog/Post.hs
@@ -17,30 +17,31 @@ data Post = Post {
       comments_open :: Bool
     } deriving (Show, Eq)
 
-addPost cn p = do { theslug <- makePostSlug cn p;
-                    p2 <- return $ p { slug = theslug };
-                    DB.doInsert cn "posts"
-                    ["title",
-                     "slug",
-                     "post_raw",
-                     "post_formatted",
-                     "summary_raw",
-                     "summary_formatted",
-                     "format_id",
-                     "timestamp",
-                     "comments_open"]
-                    [toSql $ title p2,
-                     toSql $ slug p2,
-                     toSql $ post_raw p2,
-                     toSql $ post_formatted p2,
-                     toSql $ summary_raw p2,
-                     toSql $ summary_formatted p2,
-                     toSql $ format_id p2,
-                     toSql $ timestamp p2,
-                     toSql $ comments_open p2];
-                    [[newid]] <- quickQuery cn "SELECT last_insert_rowid();" [];
-                    return p2 { uid = fromSql $ newid } ;
-                  }
+addPost cn p = do theslug <- makePostSlug cn p
+                  let p2 = p { slug = theslug }
+                  DB.doInsert cn "posts" [
+                         "title",
+                         "slug",
+                         "post_raw",
+                         "post_formatted",
+                         "summary_raw",
+                         "summary_formatted",
+                         "format_id",
+                         "timestamp",
+                         "comments_open"
+                        ] [
+                         toSql $ title p2,
+                         toSql $ slug p2,
+                         toSql $ post_raw p2,
+                         toSql $ post_formatted p2,
+                         toSql $ summary_raw p2,
+                         toSql $ summary_formatted p2,
+                         toSql $ format_id p2,
+                         toSql $ timestamp p2,
+                         toSql $ comments_open p2
+                        ]
+                  [[newid]] <- quickQuery cn "SELECT last_insert_rowid();" []
+                  return p2 { uid = fromSql $ newid }
 
 makePostSlug cn p = makeSlugGeneric cn (title p) "posts"
 

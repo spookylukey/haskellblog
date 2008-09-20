b525a51b70403b576dc2fae7c68801963f80dd7a
Luke Plant <L.Plant.98@cantab.net>
2008-09-20 14:15:06 +0100

    Tweaked imports

--- testsuite/tests/Tests/Web/Request.hs
+++ testsuite/tests/Tests/Web/Request.hs
@@ -3,10 +3,11 @@ module Tests.Web.Request
 where
 
 import qualified Web.Request as R
+import Web.Request (mkRequest)
 import Test.HUnit
 
-testMethod = "GET" ~=? R.method (R.mkRequest [("REQUEST_METHOD","GET")] "")
-testPath = "/foo/bar" ~=? R.path (R.mkRequest [("PATH_INFO", "/foo/bar")] "")
+testMethod = "GET" ~=? R.method (mkRequest [("REQUEST_METHOD","GET")] "")
+testPath = "/foo/bar" ~=? R.path (mkRequest [("PATH_INFO", "/foo/bar")] "")
 
 tests = test [
          testMethod,

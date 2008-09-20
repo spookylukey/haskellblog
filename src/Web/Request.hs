bd624747a93272e9b120a29840b4a07a937b5395
Luke Plant <L.Plant.98@cantab.net>
2008-09-20 14:38:15 +0100

    Added environment to Request, made Request abstract

--- src/Web/Request.hs
+++ src/Web/Request.hs
@@ -1,16 +1,19 @@
-module Web.Request
+module Web.Request (method, path, environment, mkRequest)
 
 where
 
 import qualified Data.Map as Map
 import Data.Maybe
 
-data Request = Request { method :: String,
-                         path :: String}
+data Request = Request { method :: String
+                       , path :: String
+                       , environment :: Map.Map String String
+                       }
 
 mkRequest :: [(String, String)] -> String -> Request
 mkRequest env stdin = let envMap = Map.fromList env
                       in Request { 
-                               method = fromJust $ Map.lookup "REQUEST_METHOD" envMap,
-                               path = fromJust $ Map.lookup "PATH_INFO" envMap
+                               method = fromJust $ Map.lookup "REQUEST_METHOD" envMap
+                             , path = fromJust $ Map.lookup "PATH_INFO" envMap
+                             , environment = envMap
                              }

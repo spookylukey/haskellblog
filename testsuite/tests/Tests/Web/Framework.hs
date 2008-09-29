89cd373b1ea72d82984650a4cc4b77286f75e9b0
Luke Plant <L.Plant.98@cantab.net>
2008-09-29 22:47:36 +0100

    Removed some unnecessary do notation

--- testsuite/tests/Tests/Web/Framework.hs
+++ testsuite/tests/Tests/Web/Framework.hs
@@ -8,6 +8,7 @@ import Web.Framework
 import Web.Request
 import Web.Response
 import Data.Maybe (isNothing)
+import Control.Monad (liftM)
 
 req1 = mkRequest [("REQUEST_METHOD","GET")] ""
 resp1 = buildResponse utf8HtmlResponse [ addContent "resp1" ]
@@ -17,14 +18,12 @@ alwaysFailView = const (return Nothing)
 alwaysSucceedView1 = const (return $ Just resp1)
 alwaysSucceedView2 = const (return $ Just resp2)
 
-testDispatchRequest1 = (do
-                         resp <- dispatchRequest req1 []
-                         return $ isNothing resp)
+testDispatchRequest1 = (dispatchRequest req1 [] 
+                        >>= return . isNothing)
                        ~? "With no views, nothing is dispatched"
 
-testDispatchRequest2 = (do
-                         resp <- dispatchRequest req1 [alwaysFailView]
-                         return $ isNothing resp)
+testDispatchRequest2 = (dispatchRequest req1 [alwaysFailView] 
+                        >>= return . isNothing)
                        ~? "Should get Nothing if all view return Nothing"
 
 testDispatchRequest3 = (do

985888b4b8aec7d59ec4f6a0c1e2ca731dded7f3
Luke Plant <L.Plant.98@cantab.net>
2008-10-06 16:18:45 +0100

    Switched to record syntax data structure for template variables instead of multiple parameters

--- src/Blog/Templates.hs
+++ src/Blog/Templates.hs
@@ -1,10 +1,17 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
 module Blog.Templates
 where
 
 import Text.XHtml
 
+
+data PageVars t1 t2 = (HTML t1, HTML t2) => PageVars
+    { ptitle :: t1
+    , pcontent :: t2
+    }
+
 -- Complete page template
-page ptitle pcontent =
+page vars =
     header
     << (meta ! [httpequiv "Content-Type",
                 content "text/html; charset=utf-8"]
@@ -18,21 +25,23 @@ page ptitle pcontent =
         +++ thelink ! [rel "shortcut icon",
                        href "/favicon.ico",
                        thetype "image/x-icon"] << ""
-        +++ (thetitle << ptitle)
+        +++ (thetitle << ptitle vars)
        )
     +++
     body
     << thediv ! [identifier "container"]
-           << pcontent
+           << pcontent vars
 
 
 -- Page specific templates
 
-mainIndexPage = page "All Unkept"
-                (thediv ! [identifier "maintitle"]
-                            << thediv
-                                   << "All Unkept"
-                 +++
-                 thediv ! [identifier "content"]
-                            << h1 << "This is the title"
-                            +++ p << "This is a test")
+mainIndexPage = page $ PageVars
+                { ptitle = "All Unkept"
+                , pcontent = (thediv ! [identifier "maintitle"]
+                              << thediv
+                                     << "All Unkept"
+                              +++
+                              thediv ! [identifier "content"]
+                                         << h1 << "This is the title"
+                              +++ p << "This is a test")
+                }

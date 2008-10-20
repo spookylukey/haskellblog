81e92d46b0d66132f60459dc6aaa8bca5f49fee5
Luke Plant <L.Plant.98@cantab.net>
2008-10-20 20:02:28 +0100

    Added docs for processors

--- src/Web/Processors/General.hs
+++ src/Web/Processors/General.hs
@@ -8,7 +8,33 @@ import Data.List (isSuffixOf)
 import Web.Request
 import Web.Response
 
--- TODO -- need to include query string, and think about how to handle
+-- ** View processors
+
+--  These take a view function and return a view function.
+--  Alternatively, take a view function and a request and return an IO
+--  (Maybe Response).  This allows them to do both request
+--  pre-processing and response post-processing.  They will be usually
+--  be used as \'decorators\' when defining routes.
+
+-- ** View functions
+
+--  These are straightforward view functions which happen to work as a
+--  kind of pre-handler.  They are installed using routes, usually
+--  before all the others.  These usually do redirects, for example
+--  addSlashRedirectProcessor
+
+-- ** Response processors
+
+--  These take a response and the original request object, and return
+--  a possibly modified response.  This can be useful for
+--  post-processing, or adding headers etc.
+
+
+-- | Returns a responseRedirect if the the request URI does not end
+-- with a slash.  Should be installed before all other routes.
+
+-- TODO
+-- need to include query string, and think about how to handle
 -- POSTs etc
 addSlashRedirectProcessor view req =
     let uri = requestUriRaw req

ec612dd1d642dfcdecd1cfa1b81d0dc497bbcc0d
Luke Plant <L.Plant.98@cantab.net>
2008-11-24 23:15:52 +0000

    Code reorganisation and comments

--- src/Blog/Views.hs
+++ src/Blog/Views.hs
@@ -11,16 +11,33 @@ import Blog.Links
 import Blog.DB (connect)
 import Blog.Model (getPostBySlug, getCategoriesForPost, getRecentPosts, getCommentsForPost, getRelatedPosts)
 
+
+---- Utilities
+
+-- | Generate a standard response, given the HTML to add.
 standardResponse html = buildResponse [
                          addHtml html
                         ] utf8HtmlResponse
 
+
+-- | Custom 404 response
+custom404 = with (standardResponse custom404page) [
+             setStatus 404
+            ]
+
+-- Temporary view
+dummyView req = return $ Just $ standardResponse ("TODO" :: String) :: IO (Maybe Response)
+
+---- Views
+
+-- View for the main page
 mainIndex :: Request -> IO (Maybe Response)
 mainIndex req = do
   cn <- connect
   posts <- getRecentPosts cn
   return $ Just $ standardResponse $ mainIndexPage posts
 
+-- View to help with debugging
 debug path req = return $ Just $ buildResponse [
                   addContent "Path:\n"
                  , addContent $ utf8 path
@@ -28,17 +45,16 @@ debug path req = return $ Just $ buildResponse [
                  , addContent $ utf8 $ show req
                  ] utf8TextResponse
 
+-- View that performs redirect to main page
 postsRedirectView req = return $ Just $ redirectResponse indexUrl :: IO (Maybe Response)
 
-custom404 = with (standardResponse custom404page) [
-             setStatus 404
-            ]
-
-dummyView req = return $ Just $ standardResponse ("TODO" :: String) :: IO (Maybe Response)
-
+-- View that shows an overview of categories
 categoriesView req = return $ Just $ standardResponse categoriesPage :: IO (Maybe Response)
+
+-- View that shows posts for an individual category
 categoryView slug = dummyView
 
+-- View that shows individual post
 postView slug req = do
   cn <- connect
   mp <- getPostBySlug cn slug

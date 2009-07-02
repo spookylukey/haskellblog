05d946d367403467bb42b1c5054182956b2ef51b
Luke Plant <L.Plant.98@cantab.net>
2009-07-02 23:53:50 +0100

    Removed unused imports

--- src/Blog/Category.hs
+++ src/Blog/Category.hs
@@ -3,9 +3,6 @@ module Blog.Category where
 
 import Data.Typeable
 import Data.Data
-import Database.HDBC
-import Blog.DBUtils (makeSlugGeneric)
-import qualified Blog.DB as DB
 
 data Category = Category { uid :: Int,
                            name :: String,

afa373f07d3fad7fd2bd78ab005c427e3189d301
Luke Plant <L.Plant.98@cantab.net>
2009-11-07 18:36:38 +0000

    Smaller feeds

--- src/Blog/settingslocal.hs
+++ src/Blog/settingslocal.hs
@@ -14,8 +14,8 @@ admin_usernames = [blog_author_name]
 
 post_page_size = 20 :: Int
 admin_post_page_size = 100 :: Int
-feed_post_page_size = 20 :: Int
-feed_comment_page_size = 20 :: Int
+feed_post_page_size = 8 :: Int
+feed_comment_page_size = 15 :: Int
 domain = "lukeplant_local"
 absolute_root = "http://" ++ domain
 

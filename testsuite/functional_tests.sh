bd581109851085e32618582b5f8e6c13f347fe6b
Luke Plant <L.Plant.98@cantab.net>
2009-11-07 00:15:57 +0000

    Beginnings of some basic functional/integration tests.

---
+++ testsuite/functional_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# These depend on the migration having been done
+
+# Check that a unicode char in a post came out right.
+curl http://lukeplant_local/blog/posts/building-ghc-is-fun/ 2> /dev/null | grep 'bug #3639</a> — it won' > /dev/null || { echo "Problem with unicode chars in posts"; }
+# and one in a comment
+curl http://lukeplant_local/blog/posts/haskell-string-support/ 2> /dev/null | grep '&nbsp;&nbsp;&nbsp;&nbsp;s = &quot;λ&quot;' > /dev/null || { echo "Problem with unicode chars in comments"; }
